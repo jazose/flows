45de8e8207d4a0e8c2374b3fa493de0165432bc5
jazose <jazose+github@gmail.com>
2013-08-13 14:41:11 -0700

    Model 6

---
+++ model6.bug.R
@@ -0,0 +1,20 @@
+model {
+  for(i in 1:n){
+    f[i] ~ dpois(lambda[i])
+    lambda[i] <- exp(alpha1*log(hist[i]+1)+alpha2*log(o[i])+alpha3*log(d[i])+
+                       delta1*dist1[i]+delta2*dist2[i]+delta3*dist3[i]+delta4*dist4[i]+
+                       delta5*dist5[i]+delta6*dist6[i]+delta7*dist7[i]+
+                       beta)
+  }
+  alpha1 ~ dunif(0,10)
+  alpha2 ~ dunif(-10,10)
+  alpha3 ~ dunif(-10,10)
+  delta1 ~ dunif(-10,10)
+  delta2 ~ dunif(-10,10)
+  delta3 ~ dunif(-10,10)
+  delta4 ~ dunif(-10,10)
+  delta5 ~ dunif(-10,10)
+  delta6 ~ dunif(-10,10)
+  delta7 ~ dunif(-10,10)
+  beta ~ dunif(-100,100)
+}
